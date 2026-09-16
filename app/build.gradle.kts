bfb17809552d282bf804ba062964c0bbc72350e7
userc0d3x <thur.sales.campos@gmail.com>
2026-09-15 22:26:33 -0300

    Adiciona versão 0.2 para atualizações do app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
+
 android {
     namespace = "com.arthur.agente"
     compileSdk = 35
@@ -11,8 +12,8 @@ android {
         applicationId = "com.arthur.agente"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
     }
 
     compileOptions {
